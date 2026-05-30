b8388a89592334c3f1271ac4b7bf1b6b8e7e9aa3
rumarsi <rumarsi@gmail.com>
2026-05-30 21:29:36 +0000

    feat: add dice rolling function and main script

---
+++ app.py
@@ -0,0 +1,19 @@
+import dice
+import time
+
+
+def lanzar_dados(amount, sides):
+    resultados = []
+
+    for _ in range(amount):
+        tirada = dice.roll(f"1d{sides}")
+        resultados.append(tirada[0])
+
+    return resultados
+
+
+resultados = lanzar_dados(amount=5, sides=6)
+
+for i, resultado in enumerate(resultados, start=1):
+    print(f"Lanzamiento {i} número obtenido {resultado}")
+    time.sleep(5)
